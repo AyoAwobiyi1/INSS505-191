3e447bd9fe0b2f65d6c16fa4b752cdd7b6421e6d
AyoAwobiyi1 <124638341+AyoAwobiyi1@users.noreply.github.com>
2023-10-15 23:28:34 -0400

    https://github.com/AyoAwobiyi1/INSS505-191.git

---
+++ Class.py
@@ -0,0 +1,4 @@
+Input1 = 3
+Input2 = 7
+Output = Input1 + Input2
+print(Input1,Input2,Output)
---
+++ Lab2.py
@@ -0,0 +1,3 @@
+print ('Ayomide Awobiyi')
+print ('Masters in Mnagement Information Systems')
+print ('I am a Consultant')
