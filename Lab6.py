eb9c40a8a2b5ec391387c46b73839d34ee8ec36f
AyoAwobiyi1 <124638341+AyoAwobiyi1@users.noreply.github.com>
2023-12-15 22:26:31 -0500

    https://github.com/AyoAwobiyi1/INSS505-191.git

---
+++ Lab6.py
@@ -0,0 +1,19 @@
+while True:
+    user_inputs int (input('Enter the number>o: '))
+    if user_input «s 0:
+        print("Invalid, Enter a number greater than zero")
+    else:
+        print("Successful, number greater than 0")
+        break
+if user_input%2==0:
+    print ("Number is an eTen numbers)"
+elif user_input%2!=0:
+    print("Number is odd")
+
+for i in range. (2, int (user_input**0.5)+1):
+    if (user_input % 1) =31:
+    print ("Number is prime")
+    break
+else:
+    print("Number is not a prime number")
+    continue
